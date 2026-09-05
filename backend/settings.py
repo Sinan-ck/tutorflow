76f92a7283541c421ce9075eb411cde2b89da60e
mohammed <mohammedsinan123ck@gmail.com>
2026-09-05 12:03:12 +0530

    Add STATIC_ROOT for production static files

--- backend/settings.py
+++ backend/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Email
@@ -170,3 +171,5 @@ CORS_ALLOWED_ORIGINS = [
 
 # --- Email (console backend prints emails to the terminal -- fine for dev/grading) ---
 DEFAULT_FROM_EMAIL = "TutorFlow <no-reply@tutorflow.local>"
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
